439e7e158b9de754e78a92836e39bba5297083e7
RicardoNieblas <ricardo.nieblas@cetys.edu.mx>
2024-07-12 23:37:39 -0700

    Created basic structure for menu view

---
+++ Mi Micro/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Mi Micro
+//
+//  Created by Ricardo Nieblas on 12/07/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
---
+++ Mi Micro/RoutesView.swift
@@ -0,0 +1,18 @@
+//
+//  RoutesView.swift
+//  Mi Micro
+//
+//  Created by Ricardo Nieblas on 12/07/24.
+//
+
+import SwiftUI
+
+struct RoutesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoutesView()
+}
